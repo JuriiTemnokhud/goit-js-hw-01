08264fe6d5a0009f6adc777b4f4de1d72584d615
JuriiTemnokhud <60181144+JuriiTemnokhud@users.noreply.github.com>
2020-04-18 16:47:06 +0300

    first commit10

---
+++ module-01/goit-JS-01-HW/js/task-02.js
@@ -0,0 +1,11 @@
+const total = 100;
+
+let ordered = 50;
+
+console.log(total, ordered);
+
+if (total <= ordered) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
